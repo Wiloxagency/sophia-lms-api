d75e18826e6a6f6914df4ede73c82022b8216446
Leonardo Barreto <edufactory@outlook.com>
2023-02-17 19:01:12 -0600

    Update image

--- Image/index.ts
+++ Image/index.ts
@@ -1,7 +1,13 @@
+import axios, { AxiosResponse } from 'axios'
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { createConnection } from "../shared/mongo";
+import { BlobServiceClient } from '@azure/storage-blob'
+import { createConnection } from "../shared/mongo"
+import sharp = require('sharp')
+import { v4 as uuidv4 } from 'uuid'
+
 
 const database = createConnection()
+const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
 
@@ -9,14 +15,28 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         courseCode: string,
         sectionIndex: number,
         elementIndex: number,
-        slideIndex: number) => {
+        slideIndex: number,
+        externalUrlImage: string) => {
 
         try {
+
+            const input = (await axios({ url: externalUrlImage, responseType: "arraybuffer" })).data as Buffer;
+            const output = await sharp(input)
+                .resize(1200, 675)
+                .jpeg()
+                .toBuffer();
+
+            const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+            const containerClient = blobServiceClient.getContainerClient("images");
+            const blobName = uuidv4() + ".jpeg"
+            const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+            await blockBlobClient.upload(output, output.length);
+            const imageField = `sections.${sectionIndex}.elements.${elementIndex}.paragraphs.${slideIndex}.imageData.finalImage.url`
             const db = await database
             const Courses = db.collection('course')
-            const resp = Courses.findOneAndUpdate({code: courseCode}, {
+            const resp = Courses.findOneAndUpdate({ code: courseCode }, {
                 $set: {
-                    "sections[sectionIndex].elements[elementIndex]": "URL testing..."
+                    [imageField]: blockBlobClient.url
                 }
             })
 
@@ -35,7 +55,8 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                 req.body.courseCode,
                 req.body.sectionIndex,
                 req.body.elementIndex,
-                req.body.slideIndex)
+                req.body.slideIndex,
+                req.body.externalUrlImage)
             break;
 
         case "GET":
