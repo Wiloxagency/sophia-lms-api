edf970b605a6dff754bb3ccc9d9e35045ff6a2eb
Leo <LeoLeto@protonmail.com>
2023-02-21 23:26:18 -0300

    Get users

--- User/index.ts
+++ User/index.ts
@@ -84,7 +84,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                 userAggregation(userCode)
             )
             const body = await resp.toArray()
-            if (body && body.length > 0 ) {
+            if (body && body.length > 0) {
                 context.res = {
                     "status": 200,
                     "headers": {
@@ -101,7 +101,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                 }
 
             }
-            
+
         } catch (error) {
 
         }
@@ -110,7 +110,30 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     }
 
     const getUsers = async () => {
+        try {
+            const db = await database
+            const Users = db.collection('user')
+            const resp = Users.find({})
+            const body = await resp.toArray()
+            if (body && body.length > 0) {
+                context.res = {
+                    "status": 200,
+                    "headers": {
+                        "Content-Type": "application/json"
+                    },
+                    "body": body
+                }
+            } else {
+                context.res = {
+                    "status": 204,
+                    "headers": {
+                        "Content-Type": "application/json"
+                    }
+                }
 
+            }
+        } catch (error) {
+        }
     }
 
 
