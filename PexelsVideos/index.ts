a87db38e4b86a5c38ef241e23c7ef86559a67fbe
Leo <LeoLeto@protonmail.com>
2023-03-16 22:20:12 -0300

    Post

--- PexelsVideos/index.ts
+++ PexelsVideos/index.ts
@@ -2,9 +2,11 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { createClient } from 'pexels';
 
 const client = createClient('mtCEdnRigTAFPj5nELvf5XSfjwfslcwz1qGfCf0gj1EZ57XCh3KraNns');
-const query = 'Nature';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    console.log(req.body)
+    const query = req.body.query
+    console.log(query)
     let responseVideos = await client.videos.search({ query, per_page: 80, orientation: 'landscape' })
 
     context.res = {
