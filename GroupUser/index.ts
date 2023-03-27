08feec09c0be30a36c9fdf9fc33e0ea7bc7d3643
Kelvin <kseidel@edutecno.com>
2023-03-27 08:56:47 -0300

    groupUser: check if user already exists and create

--- GroupUser/index.ts
+++ GroupUser/index.ts
@@ -21,31 +21,60 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
             const Users = db.collection('user');
             const Groups = db.collection<Group>('group');
-         
+
+            const userEmail = req.body.email;
+            const groupCode = req.params.groupCode;
             const user = req.body;
-    
+
+            const existingEmail = await Users.findOne({ 'email': userEmail });
+
+            if (existingEmail) {
+                const existingGroupUser = await Groups.findOne({ 'code': req.params.groupCode, 'users.email': userEmail });
+                if (existingGroupUser) {
+                    context.res = {
+                        "status": 400,
+                        "headers": {
+                            "Content-Type": "application/json"
+                        },
+                        "body": {
+                            "message": "This user is already in the group"
+                        }
+                    };
+                    return;
+            } else {
+                await Groups.findOneAndUpdate({ 'code': groupCode }, { $push: { users: user } });
+                context.res = {
+                    "status": 201,
+                    "headers": {
+                        "Content-Type": "application/json"
+                    },
+                    "body": {
+                        "message": "User added to group"
+                    }
+                };
+                return;
+            }
+                }
+
             const resp = await Users.insertOne(user);
     
-            const groupCode = req.params.groupCode;
-    
-            await Groups.findOneAndUpdate({ 'code': groupCode }, { $push: { users: user } },
-            );
+            await Groups.findOneAndUpdate({ 'code': groupCode }, { $push: { users: user } });
     
             context.res = {
-                status: 201,
-                headers: {
+                "status": 201,
+                "headers": {
                     "Content-Type": "application/json"
                 },
-                body: resp
+                "body": resp
             };
         } catch (error) {
             context.res = {
-                status: 500,
-                headers: {
+                "status": 500,
+                "headers": {
                     "Content-Type": "application/json"
                 },
-                body: {
-                    message: error.toString()
+                "body": {
+                    "message": error.toString()
                 }
             };
         }
