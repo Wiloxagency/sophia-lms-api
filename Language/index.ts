61d3549b4e22c460b03c05a95a5a7b3deb53b97e
Kelvin Seidel <kseidel@edutecno.com>
2023-04-22 12:57:12 -0300

    updateLanguages

--- Language/index.ts
+++ Language/index.ts
@@ -3,6 +3,7 @@ import { createConnection } from "../shared/mongo";
 import { isoLangPro } from "./isoLang";
 import { template } from "./template";
 const fs = require('fs');
+const outputFile = 'translated-values.json';
 
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
@@ -16,136 +17,158 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
     const translate = async (text: string, langIso: string) => {
         const language = isoLangPro[langIso];
-      
+
         const prompt = `###\nTranslate: "${text}" into ${language} using the following JSON format: {"Original": English phrase, "Translation": Translated Phrase} \nTranslate it in a website context and show only one translation option.\n\nExample:\n\n{"Original": "Home", "Translation": "Inicio"\n\n###\nSolution:\n{"Original": "`;
-      
+
         console.info("Prompt --> ", prompt);
-      
+
         try {
-          const openai = new OpenAIApi(configuration);
-      
-          const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: prompt,
-            temperature: 0,
-            max_tokens: 256,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-          });
-          const translation = response.data.choices[0].text;
-          console.info("Start translation -->", translation, "End translation");
-      
-          //return "{'Original':" + translation + "}" // adicionado o retorno da tradução
-          return '{"Original": "'+ translation
+            const openai = new OpenAIApi(configuration);
+
+            const response = await openai.createCompletion({
+                model: "text-davinci-003",
+                prompt: prompt,
+                temperature: 0,
+                max_tokens: 256,
+                top_p: 1,
+                frequency_penalty: 0,
+                presence_penalty: 0,
+            });
+            const translation = response.data.choices[0].text;
+            console.info("Start translation -->", translation, "End translation");
+
+            //return "{'Original':" + translation + "}" // adicionado o retorno da tradução
+            return '{"Original": "' + translation
         } catch (error) {
-          context.res = {
-            "status": 500,
-            "headers": {
-              "Content-Type": "application/json"
-            },
-            "statusText": "Can't get language"
-          }
+            context.res = {
+                "status": 500,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "statusText": "Can't get language"
+            }
         }
-      };
-      
+    };
 
-      // const translateValues = async (template, langIso, outputFile) => {
-      //   const translatedTemplate = {};
-      
-      //   for (const [key, value] of Object.entries(template)) {
-      //     if (typeof value === 'string') {
-      //       if (value.includes(':')) {
-      //         const parts = value.split(':');
-      //         if (parts[1]) {
-      //           translatedTemplate[key] = parts[1].trim();
-      //         } else {
-      //           translatedTemplate[key] = '';
-      //         }
-      //       } else {
-      //         const translatedValue = await translate(value, langIso);
-      //         if (typeof translatedValue === 'string') {
-      //           const rightValue = translatedValue.split(':')[1];
-      //           if (rightValue) {
-      //             const trimmedValue = rightValue.trim();
-      //             translatedTemplate[key] = trimmedValue.endsWith('.') ? trimmedValue.slice(0, -1) : trimmedValue;
-      //           } else {
-      //             translatedTemplate[key] = '';
-      //           }
-      //         }
-      //       }
-      //     } else {
-      //       translatedTemplate[key] = value;
-      //     }
-      //   }
-      
-      //   if (translatedTemplate) {
-      //     context.res = {
-      //       "status": 200,
-      //       "headers": {
-      //         "Content-Type": "application/json"
-      //       },
-      //       "body": translatedTemplate
-      //     }
-      
-      //   } else {
-      //     context.res = {
-      //       "status": 204,
-      //       "headers": {
-      //         "Content-Type": "application/json"
-      //       }
-      //     }
-      //   }
-      
-      //   try {
-      //     // await fs.writeFile(outputFile, JSON.stringify(translatedTemplate, null, 2), (err) => {
-      //     //   if (err) throw err;
-      //     //   console.log('Values saved to file');
-      //   } catch (err) {
-      //     console.error(err);
-      //   }
-      
-      //   return translatedTemplate;
-      // }
-      
-        // const outputFile = 'translated-values.json';
-        // const translatedTemplate = await translateValues(template, 'nb', outputFile);
-        // console.log(translatedTemplate);
+    const translateObjectValues = async (lang: string) => {
+        const translatedObj = {};
 
-        const putLanguages = async (lang: string, newKey: string, newValue: string) => {
-          const filePath = 'languages/pt2.json';
-          const newValueTranslated = newValue;
-          const newTranslated = await translate(newValueTranslated, lang);
-          console.info("newTranslated", newTranslated)
-          const newTranslatedWord = JSON.parse(newTranslated)["Translation"]
-          const jsonData = await fs.readFileSync(filePath, 'utf8');
-          const jsonObj = JSON.parse(jsonData);
-          const es = jsonObj;
+        for (const [key, value] of Object.entries(template)) {
+            const translatedValue = await translate(value, lang);
+            translatedObj[key] = JSON.parse(translatedValue).Translation;
+        }
 
-          console.info('>>>>>>' + JSON.stringify(es))
-          
-          es[newKey] = newTranslatedWord;
-          const esJSON = JSON.stringify(es, null, 2);
+        try {
+            await fs.writeFile(outputFile, JSON.stringify(translatedObj, null, 2), (err) => {
+                if (err) throw err;
+                console.log('Values saved to file');
+            })
+        } catch (err) {
+            console.error(err);
+        }
 
-          console.info('>>>>>>' + esJSON)
-          
-          try {
+        return translatedObj;
+    }
+
+
+
+    // const translateValues = async (template, lang, outputFile) => {
+    //     const translatedTemplate = {};
+
+    //     for (const [key, value] of Object.entries(template)) {
+    //         if (typeof value === 'string') {
+    //             if (value.includes(':')) {
+    //                 const parts = value.split(':');
+    //                 if (parts[1]) {
+    //                     translatedTemplate[key] = parts[1].trim();
+    //                 } else {
+    //                     translatedTemplate[key] = '';
+    //                 }
+    //             } else {
+    //                 const translatedValue = await translate(value, lang);
+    //                 if (typeof translatedValue === 'string') {
+    //                     const rightValue = translatedValue.split(':')[1];
+    //                     if (rightValue) {
+    //                         const trimmedValue = rightValue.trim();
+    //                         translatedTemplate[key] = trimmedValue.endsWith('.') ? trimmedValue.slice(0, -1) : trimmedValue;
+    //                     } else {
+    //                         translatedTemplate[key] = '';
+    //                     }
+    //                 }
+    //             }
+    //         } else {
+    //             translatedTemplate[key] = value;
+    //         }
+    //     }
+
+    //     if (translatedTemplate) {
+    //         context.res = {
+    //             "status": 200,
+    //             "headers": {
+    //                 "Content-Type": "application/json"
+    //             },
+    //             "body": translatedTemplate
+    //         }
+
+    //     } else {
+    //         context.res = {
+    //             "status": 204,
+    //             "headers": {
+    //                 "Content-Type": "application/json"
+    //             }
+    //         }
+    //     }
+
+    //     try {
+    //         await fs.writeFile(outputFile, JSON.stringify(translatedTemplate, null, 2), (err) => {
+    //             if (err) throw err;
+    //             console.log('Values saved to file');
+    //         })
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+
+    //     return translatedTemplate;
+    // }
+
+
+    // const translatedTemplate = await translateValues(template, req.params.lang, outputFile);
+    // console.log(translatedTemplate);
+
+    const updateLanguages = async (lang: string, newKey: string, newValue: string) => {
+        const filePath = 'languages/pt.json';
+        const newValueTranslated = newValue;
+        const newTranslated = await translate(newValueTranslated, lang);
+        console.info("newTranslated", newTranslated)
+        const newTranslatedWord = JSON.parse(newTranslated)["Translation"]
+        const jsonData = await fs.readFileSync(filePath, 'utf8');
+        const jsonObj = JSON.parse(jsonData);
+        const es = jsonObj;
+
+        console.info('>>>>>>' + JSON.stringify(es))
+
+        es[newKey] = newTranslatedWord;
+        const esJSON = JSON.stringify(es, null, 2);
+
+        console.info('>>>>>>' + esJSON)
+
+        try {
             await fs.promises.writeFile(filePath, esJSON, 'utf8');
             console.log('Alterações salvas com sucesso.');
-          } catch (err) {
+        } catch (err) {
             console.error(err);
             throw new Error('Erro ao gravar as alterações no arquivo JSON.');
-          }
-          
-          return es;
-        };
-        
-        // const newPhraseTranslated = await putLanguages('es', 'new key', 'new value');
-        // console.log(newPhraseTranslated);
-        
-        
-        
-      
+        }
+
+        return es;
+    };
+
+    // const newPhraseTranslated = await putLanguages('es', 'new key', 'new value');
+    // console.log(newPhraseTranslated);
+
+
+
+
 
     const getLanguages = async (lang: string) => {
 
@@ -192,7 +215,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         }
     }
 
-      
+
 
     const buildLanguagePackage = async (text: string, lang: string) => {
 
@@ -254,10 +277,11 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             await getLanguages(req.params.lang)
             break;
         case "POST":
-            await buildLanguagePackage(req.body.text, req.params.lang)
+            // await buildLanguagePackage(req.body.text, req.params.lang)
+            await translateObjectValues(req.params.lang);
             break;
         case "PUT":
-            await putLanguages(req.params.lang, req.body.newKey, req.body.newValue)
+            await updateLanguages(req.params.lang, req.body.newKey, req.body.newValue)
             break;
 
 
