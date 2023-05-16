ce4589562aa1801dbedd4c68dd45ceff27306847
Leonardo Barreto <leonardojbarreto@gmail.com>
2023-05-16 11:48:25 -0300

    GPT4

--- Gpt4/index.ts
+++ Gpt4/index.ts
@@ -12,6 +12,21 @@ const openai = new OpenAIApi(configuration)
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
 
     try {
+        // const response = await openai.createChatCompletion({
+        //     model: "gpt-4",
+        //     messages: [
+        //         {
+        //             role: "system",
+        //             content: 'You are a helpful assistant.'
+        //         },
+        //         {
+        //             role: "user",
+        //             content: req.body.prompt
+        //         }
+        //     ]
+        // })
+
+        console.info(req.body.prompt)
         const response = await openai.createChatCompletion({
             model: "gpt-4",
             messages: [
@@ -23,8 +38,12 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                     role: "user",
                     content: req.body.prompt
                 }
-            ]
-        })
+            ],
+            temperature: req.body.temperature,
+            max_tokens: req.body.max_tokens
+
+          });
+
         // console.log(response.data.choices[0].message.content)
         context.res = {
             "status": 200,
@@ -32,6 +51,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                 "Content-Type": "application/json"
             },
             "body": response.data.choices[0].message.content
+            
         }
     } catch (error) {
         await saveLog(`Error creating Chat Completion, error: ${error.message} `, "Error", "AzureFunction()", "GPT")
