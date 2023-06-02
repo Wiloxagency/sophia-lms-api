de14699391379e2f3009d1d38d0f31b252cc6837
Kelvin Seidel <seidelkelvin@gmail.com>
2023-06-02 14:14:49 -0300

    add Scorm and refactor textElement

--- TextElement/index.ts
+++ TextElement/index.ts
@@ -1,80 +1,109 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { createConnection } from "../shared/mongo"
-import { DocumentCreator } from "../shared/downloadElementAsDoc"
-import { Packer } from "docx"
-import { BlobServiceClient } from "@azure/storage-blob"
-import { saveLog } from "../shared/saveLog"
-import { v4 as uuidv4 } from 'uuid'
-import axios, { AxiosResponse } from 'axios'
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { createConnection } from "../shared/mongo";
+import { DocumentCreator } from "../shared/downloadElementAsDoc";
+import { Packer } from "docx";
+import { BlobServiceClient } from "@azure/storage-blob";
+import { saveLog } from "../shared/saveLog";
+import { v4 as uuidv4 } from "uuid";
+import axios, { AxiosResponse } from "axios";
 
-const database = createConnection()
-const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING
+const database = createConnection();
+const AZURE_STORAGE_CONNECTION_STRING =
+  process.env.AZURE_STORAGE_CONNECTION_STRING;
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const downloadTextElementAsDoc = async (
+    courseCode: string,
+    indexSection: string,
+    indexElement: string
+  ): Promise<string> => {
+    try {
+      // console.log(req.query)
+      const db = await database;
+      const Courses = db.collection("course");
+      let courseFindOnePromise = Courses.findOne({
+        code: req.query.courseCode,
+      });
+      let course = await courseFindOnePromise;
+      let textElement =
+        course.sections[parseInt(indexSection)].elements[parseInt(indexElement)]
+          .elementText;
 
-    const downloadTextElementAsDoc = async (courseCode: string, indexSection: string, indexElement: string) => {
-        try {
-            // console.log(req.query)
-            const db = await database
-            const Courses = db.collection('course')
-            let courseFindOnePromise = Courses.findOne({ code: req.query.courseCode })
-            let course = await courseFindOnePromise
-            let textElement = course.sections[parseInt(indexSection)].elements[parseInt(indexElement)].elementText
+      let textElementBuffer: Buffer;
+      const documentCreatorResponse = new DocumentCreator();
+      let textElementDocument: any;
+      let imageBuffer = (
+        await axios({
+          url: textElement.cover,
+          responseType: "arraybuffer",
+          timeout: 15000,
+        })
+      ).data as Buffer;
+      textElementDocument = documentCreatorResponse.createTextDocument(
+        textElement,
+        imageBuffer
+      );
 
-            let textElementBuffer: Buffer
-            const documentCreatorResponse = new DocumentCreator()
-            let textElementDocument: any
-            let imageBuffer =  (await axios({ url: textElement.cover, responseType: "arraybuffer", timeout:15000 })).data as Buffer
-            textElementDocument = documentCreatorResponse.createTextDocument(textElement, imageBuffer)
+      await Packer.toBuffer(textElementDocument).then((buffer) => {
+        // fs.writeFileSync("My Document.docx", buffer);
+        textElementBuffer = buffer;
+      });
 
-            await Packer.toBuffer(textElementDocument).then((buffer) => {
-                // fs.writeFileSync("My Document.docx", buffer);   
-                textElementBuffer = buffer
-            })
+      const blobServiceClient = BlobServiceClient.fromConnectionString(
+        AZURE_STORAGE_CONNECTION_STRING
+      );
+      const containerClient = blobServiceClient.getContainerClient("files");
+      const blobName = uuidv4() + ".docx";
+      const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+      await blockBlobClient.upload(textElementBuffer, textElementBuffer.length);
 
-            const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
-            const containerClient = blobServiceClient.getContainerClient("files");
-            const blobName = uuidv4() + ".docx"
-            const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-            await blockBlobClient.upload(textElementBuffer, textElementBuffer.length);
+      return blockBlobClient.url;
+    } catch (error) {
+      await saveLog(
+        `Error downloading text element: ${error.message}`,
+        "Error",
+        "downloadTextElementAsDoc()",
+        "TextElement"
+      );
+      context.res = {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          message: "Error",
+        },
+      };
+    }
+  };
 
-            context.res = {
-                "status": 201,
-                "headers": {
-                    "Content-Type": "application/json"
-                },
-                "body": { "url": blockBlobClient.url }
-            }
-        } catch (error) {
-            await saveLog(`Error downloading text element: ${error.message}`, "Error", "downloadTextElementAsDoc()", "TextElement")
-            context.res = {
-                "status": 500,
-                "headers": {
-                    "Content-Type": "application/json"
-                },
-                "body": {
-                    "message": "Error"
-                }
-            }
+  switch (req.method) {
+    case "POST":
+      if (req.query.operation == "download") {
+        const response = await downloadTextElementAsDoc(
+          req.query.courseCode,
+          req.query.indexSection,
+          req.query.indexElement
+        );
+        if (response) {
+          context.res = {
+            status: 201,
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: { url: response },
+          };
         }
-    }
+      }
 
-    switch (req.method) {
-        case "POST":
+      break;
 
-            if (req.query.operation == 'download') {
-                await downloadTextElementAsDoc(
-                    req.query.courseCode,
-                    req.query.indexSection,
-                    req.query.indexElement
-                )
-            }
-            break;
-
-        default:
-            break;
-    }
-
-}
+    default:
+      break;
+  }
+};
 
 export default httpTrigger;
