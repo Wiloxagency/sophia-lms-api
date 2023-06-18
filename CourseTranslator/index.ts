271626b9beb1ca3a403e1f5cb9beba9536f19f67
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2023-06-18 07:46:49 -0300

    Working base

--- CourseTranslator/index.ts
+++ CourseTranslator/index.ts
@@ -1,6 +1,10 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { createConnection } from "../shared/mongo";
 import { saveLog } from "../shared/saveLog"
 import axios, { AxiosResponse } from 'axios'
+
+const database = createConnection()
+
 
 // const COURSE_TRANSLATOR_SUBSCRIPTION_KEY = process.env.COURSE_TRANSLATOR_SUBSCRIPTION_KEY
 const COURSE_TRANSLATOR_SUBSCRIPTION_KEY = '569573b0d7c9412887eaef823b637e01'
@@ -22,40 +26,60 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         }
     ]
 
-    let params = {
-        'api-version': '3.0',
-        'to': ['sw', 'it']
-    }
-
     try {
+        const db = await database
+        const Courses = db.collection('course')
+        const findCourseResponse = Courses.findOne({ "code": req.query.courseCode })
+        const course = await findCourseResponse
+        const courseClone = JSON.parse(JSON.stringify(course))
 
-        // curl -X POST "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=zh-Hans"
-        // -H "Ocp-Apim-Subscription-Key: <client-secret>" -H "Content-Type: application/json; charset=UTF-8"
-        // -d "[{'Text':'Hello, what is your name?'}]"
+        // courseClone.details.title = await translateArray([{ Text: course.details.title }])
+        // courseClone.details.summary = await translateArray([{ Text: course.details.summary }])
+        courseClone.sections
+            .forEach((section: any, indexSection: number) => {
+                courseClone.sections[indexSection].title = indexSection + ': This section title has been translated'
 
-        const body = await axios.post(
-            'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=es',
-            tempBody,
-            requestConfiguration
-        )
-            .then(async result => { return result })
+                section.elements
+                    .forEach((element: any, indexElement: number) => {
+                        if (element.type == 'Lección Engine') {
 
-        console.log(body)
-        if (body) {
+                            course.sections[indexSection].elements[indexElement].elementLesson.paragraphs
+                                .forEach((paragraph: any, indexParagraph: number) => {
 
-            context.res = {
-                "status": 200,
-                "headers": {
-                    "Content-Type": "application/json"
-                },
-                "body": body.data
-            }
-        }
+                                    courseClone.sections[indexSection].elements[indexElement].elementLesson
+                                        .paragraphs[indexParagraph] = indexParagraph + ': This paragraph title has been translated'
+
+                                })
+
+                            console.log(course.sections[indexSection].elements[indexElement].elementLesson.paragraphs[0].content)
+                            course.sections[indexSection].elements[indexElement].elementLesson.paragraphs[0].content = ''
+                        }
+                    })
+            })
+        console.log(courseClone)
     }
     catch (error) {
         await saveLog(`Error translating course. ` + error.message, "Error", "CourseTranslator", "CourseTranslator")
     }
+}
 
+async function translateArray(arrayToTranslate: [{ Text: string }]) {
+    try {
+        const body = await axios.post(
+            'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=de',
+            arrayToTranslate,
+            requestConfiguration
+        )
+            .then(async result => {
+                return result.data[0].translations[0].text
+            })
+        // THIS IF BLOCK IS REQUIRED FOR THE ABOVE .then() TO WORK 🤷🏼‍♂️
+        if (body) {
+            return body
+        }
+    } catch (error) {
+        await saveLog(`Error translating course. ` + error.message, "Error", "translateArray()", "CourseTranslator")
+    }
 }
 
 export default httpTrigger;
