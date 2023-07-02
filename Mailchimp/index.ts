6b491ec011cd0099a797ce90dd95799b648fe1b2
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2023-07-02 00:41:55 -0300

    Static segment

--- Mailchimp/index.ts
+++ Mailchimp/index.ts
@@ -1,4 +1,8 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { v4 as uuidv } from "uuid"
+import { saveLog } from "../shared/saveLog";
+import { createHash } from 'crypto';
+
 const mailchimp = require('@mailchimp/mailchimp_marketing');
 
 mailchimp.setConfig({
@@ -7,27 +11,188 @@ mailchimp.setConfig({
 });
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const listId = "5db638bb59"
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    async function mailchimpPing() {
+        const response = await mailchimp.ping.get();
+        console.log(response);
+    }
 
-    mailchimpPing()
+    //  CAN'T CREATE ANY AUDICENCES ON THE FREE TIER
+    async function createMailList() {
+        const groupName = 'Group name ' + uuidv()
 
-};
+        const footerContactInfo = {
+            company: "Mailchimp",
+            address1: "675 Ponce de Leon Ave NE",
+            address2: "Suite 5000",
+            city: "Atlanta",
+            state: "GA",
+            zip: "30308",
+            country: "US"
+        };
 
+        const campaignDefaults = {
+            from_name: "Gettin' Together",
+            from_email: "gettintogether@example.com",
+            subject: "JS Developers Meetup",
+            language: "EN_US"
+        };
 
-async function mailchimpPing() {
-    const response = await mailchimp.ping.get();
-    console.log(response);
+        try {
+            const response = await mailchimp.lists.createList({
+                name: groupName,
+                contact: footerContactInfo,
+                permission_reminder: "permission_reminder",
+                email_type_option: true,
+                campaign_defaults: campaignDefaults
+            });
+            console.log(
+                `Successfully created an audience. The audience id is ${response.id}.`
+            );
+
+        } catch (error) {
+            await saveLog(`Error creating audience. ` + error.message, "Error", "createMailList()", "Mailchimp/")
+            context.res = {
+                "status": 500,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": {
+                    "message": error.toString()
+                }
+            }
+        }
+    }
+
+    async function addUserToList() {
+        const subscribingUser = {
+            firstName: "Prudence",
+            lastName: "McVankab",
+            email: "LeoLeto@proton.me"
+        }
+
+        try {
+            const response = await mailchimp.lists.addListMember(listId, {
+                email_address: subscribingUser.email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: subscribingUser.firstName,
+                    LNAME: subscribingUser.lastName
+                }
+            });
+
+            console.log(
+                `Successfully added contact as an audience member. The contact's id is ${response.id
+                }.`
+            );
+
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    async function unsubscribeUser() {
+        const email = "LeoLeto@proton.me"
+
+        console.log(hashText(email.toLowerCase()))
+        const subscriberHash = hashText(email.toLowerCase())
+        const response = await mailchimp.lists.updateListMember(
+            listId,
+            subscriberHash,
+            {
+                status: "unsubscribed"
+            }
+        );
+
+        console.log(`This user is now ${response.status}.`);
+    }
+
+    async function sendTestEmail() {
+        try {
+            const response = await mailchimp.campaigns.sendTestEmail("9b789d8d4a", {
+                // test_emails: ["LeoLeto@proton.me"],
+                test_emails: ["Lexp2008@gmail.com"],
+                send_type: "plaintext",
+            });
+            console.log(response);
+        } catch (error) {
+            console.log(error.status)
+            console.log(error.message)
+        }
+    }
+
+    async function sendCampaign() {
+        console.log('THIS RUNS')
+        try {
+            const response = await mailchimp.campaigns.send("9b789d8d4a")
+            console.log(response)
+        } catch (error) {
+            await saveLog(`Error sending campaign. ` + error.message, "Error", "sendCampaign()", "Mailchimp/")
+            context.res = {
+                "status": 500,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": {
+                    "message": error.toString()
+                }
+            }
+        }
+    }
+
+    async function createStaticSegment() {
+        let emails = ['LeoLeto@proton.me']
+        try {
+            const response = await mailchimp.lists.createSegment(listId, {
+                name: uuidv(),
+                static_segment: emails
+            })
+            console.log(response)
+        } catch (error) {
+            await saveLog(`Error creating segmen. ` + error.message, "Error", "createStaticSegment()", "Mailchimp/")
+            context.res = {
+                "status": 500,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": {
+                    "message": error.toString()
+                }
+            }
+        }
+    }
+
+    function hashText(text) {
+        const hashFunc = createHash('md5');   // you can also sha256, sha512 etc
+        hashFunc.update(text);
+        return hashFunc.digest('hex');       // will return hash, formatted to HEX
+    }
+
+    switch (req.query.operation) {
+        case 'ping':
+            mailchimpPing()
+            break
+        case 'createMailList':
+            createMailList()
+            break
+        case 'addUserToList':
+            addUserToList()
+            break
+        case 'unsubscribeUser':
+            unsubscribeUser()
+            break
+        case 'sendTestEmail':
+            sendTestEmail()
+            break
+        case 'sendCampaign':
+            sendCampaign()
+            break
+        case 'createStaticSegment':
+            createStaticSegment()
+            break
+    }
+
 }
 
-
-
-export default httpTrigger;
+export default httpTrigger
