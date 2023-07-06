d635022decdbb57d2dd895f7fdb46cd493dc6fac
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2023-07-05 22:40:53 -0300

    For loop

--- nodemailer/index.ts
+++ nodemailer/index.ts
@@ -1,5 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import * as aws from "@aws-sdk/client-ses";
+import path = require("path");
+import handlebars = require("handlebars");
 
 const fs = require('fs');
 const { promisify } = require('util');
@@ -20,30 +22,59 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     })
 
     const transporter = nodemailer.createTransport({
-        SES: { ses, aws }
+        SES: { ses, aws },
+        sendingRate: 14
     })
 
+    const testUsers = [
+        {
+            "name": "Leo Leto",
+            "email": "LeoLeto@proton.me"
+        }, {
+            "name": "Leonardo José",
+            "email": "Leonardojbarreto@gmail.com"
+        }, {
+            "name": "Leonardo Daniel",
+            "email": "Lexp2008@gmail.com"
+        }
+    ]
+
     async function sendEmail() {
-        try {
-            const info = await transporter.sendMail({
-                from: '"Sophia" <hola@iasophia.com>',
-                to: [],
-                bcc: "LeoLeto@protonmail.com",
-                subject: "Hello from Sophia 🎓",
-                // text: "Hello world", 
-                html: await readFile('nodemailer/welcome.html', 'utf8'), //
-            })
-            if (info) {
-                console.log(info)
-                return
-                context.res = {
-                    // status: 200, /* Defaults to 200 */
-                    body: info
+        let htmlToSend
+
+        if (req.query.emailTemplate == 'welcome') {
+            const source = fs.readFileSync('nodemailer/welcome.html', 'utf-8').toString()
+            const template = handlebars.compile(source)
+            for (const [indexUser, user] of testUsers.entries()) {
+            // for (const [indexUser, user] of req.body.entries()) {
+                const replacements = {
+                    username: user.name
+                }
+                htmlToSend = template(replacements)
+
+                try {
+                    const info = await transporter.sendMail({
+                        from: '"Sophia" <hola@iasophia.com>',
+                        to: user.email,
+                        // bcc: "LeoLeto@protonmail.com, Lexp2008@gmail.com, Leonardojbarreto@gmail.com",
+                        subject: "Hello from Sophia 🎓",
+                        // text: "Hello world", 
+                        // html: await readFile('nodemailer/welcome.html', 'utf8'), 
+                        html: htmlToSend
+                    })
+                    if (info) {
+                        context.res = {
+                            // status: 200, /* Defaults to 200 */
+                            body: info
+                        }
+                    }
+                } catch (error) {
+                    console.log(error)
                 }
             }
-        } catch (error) {
-            console.log(error)
+
         }
+
     }
 
     sendEmail()
