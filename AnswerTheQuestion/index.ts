6c1b9b43676c974535781fbffe5a8bbcd522fed2
Kelvin Seidel <seidelkelvin@gmail.com>
2023-08-21 17:36:22 -0300

    text course

--- AnswerTheQuestion/index.ts
+++ AnswerTheQuestion/index.ts
@@ -1,8 +1,13 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { Configuration, OpenAIApi } from "openai";
 import { saveLog } from "../shared/saveLog";
+import { createConnection } from "../shared/mongo";
 
-// OpenAI Credentials
+const AZURE_STORAGE_CONNECTION_STRING =
+  process.env.AZURE_STORAGE_CONNECTION_STRING;
+
+const database = createConnection();
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -13,59 +18,81 @@ const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  try {
-    // const response = await openai.createChatCompletion({
-    //     model: "gpt-4",
-    //     messages: [
-    //         {
-    //             role: "system",
-    //             content: 'You are a helpful assistant.'
-    //         },
-    //         {
-    //             role: "user",
-    //             content: req.body.prompt
-    //         }
-    //     ]
-    // })
+  const getCourse = async (courseCode: string) => {
+    try {
+      const db = await database;
+      const Courses = db.collection("course");
 
-    console.info(req.body.prompt);
+      const resp = Courses.aggregate([
+        {
+          $match: {
+            code: courseCode,
+          },
+        },
+        {
+          $lookup: {
+            from: "user",
+            localField: "author_code",
+            foreignField: "code",
+            as: "createdBy",
+          },
+        },
+        {
+          $addFields: {
+            createdBy: "$createdBy.name",
+          },
+        },
+        {
+          $unwind: {
+            path: "$createdBy",
+          },
+        },
+      ]);
+
+      const body = await resp.toArray();
+
+      if (body && body[0]) {
+        console.log(
+          "Retrieved course:",
+          body[0].sections[0].elements[0].elementLesson.paragraphs[0].content
+        );
+      } else {
+        console.log("Course not found");
+      }
+    } catch (error) {
+      console.error("Error getting course:", error.message);
+    }
+  };
+
+  try {
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo-16k",
       messages: [
-        {
-          role: "system",
-          content: req.body.role,
-        },
-        {
-          role: "user",
-          content: req.body.prompt,
-        },
+        { role: "system", content: req.body.role },
+        { role: "user", content: req.body.prompt },
       ],
     });
 
-    // console.log(response.data.choices[0].message.content)
+    const extractedValue = response.data.choices[0].message.content;
+
+    await getCourse(extractedValue);
+
     context.res = {
       status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: response.data.choices[0].message.content,
     };
   } catch (error) {
     await saveLog(
-      `Error creating Chat Completion, error: ${error.message} `,
+      `Error creating Chat Completion: ${error.message}`,
       "Error",
       "AzureFunction()",
       "GPT"
     );
     context.res = {
       status: 500,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: {
-        message: "Error",
-      },
+      headers: { "Content-Type": "application/json" },
+      body: { message: "Error" },
     };
   }
 };
