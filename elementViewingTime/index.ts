218b7ae7f15495bd7733720d21aaffef7f849de9
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2023-08-23 04:33:01 -0300

    Working progress

--- elementViewingTime/index.ts
+++ elementViewingTime/index.ts
@@ -12,9 +12,35 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         const db = await database
         const Users = db.collection('user')
         const fetchedUser = await Users.findOne({ code: req.body.studentCode })
+        const Courses = db.collection('course')
+        const fetchedCourse = await Courses.findOne({ code: req.body.courseCode })
+        // console.log(req.body)
+        // console.log(fetchedCourse)
+        let courseElementsCounter: number = 0
+        let elementViewingTimeRegistriesCounter: number = 0
+
+        for (let section of fetchedCourse.sections) {
+            for (let element of section.elements) {
+                if (
+                    element.type == 'Lección Engine' ||
+                    element.type == 'file' ||
+                    element.type == 'video_url' ||
+                    element.type == 'html'
+                ) {
+                    courseElementsCounter++
+                }
+            }
+        }
+        // console.log(courseElementsCounter)
+
         let indexGroup = fetchedUser.groups.findIndex((group: any) => group.groupCode === req.body.groupCode)
         const currentGroup = fetchedUser.groups[indexGroup]
         let elementTimesArrayPath = `groups.${indexGroup}.elementTimes`
+
+        if (currentGroup.elementTimes) {
+            elementViewingTimeRegistriesCounter = currentGroup.elementTimes.length
+            // console.log(elementViewingTimeRegistriesCounter)
+        }
 
         let elementTimesPayload: any =
         {
@@ -30,6 +56,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                     [elementTimesArrayPath]: [elementTimesPayload]
                 }
             })
+            elementViewingTimeRegistriesCounter = 1
             // console.log(updateGroupResponse)
         }
         else {
@@ -53,6 +80,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                         [elementTimesArrayPath]: elementTimePayload
                     }
                 })
+                elementViewingTimeRegistriesCounter++
                 // console.log(updateGroupResponse)
             } else {
                 let singleElementTimePath = `groups.${indexGroup}.elementTimes.${indexElementTime}.time`
@@ -64,6 +92,21 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                 // console.log(updateGroupResponse)
             }
         }
+
+        let courseProgress = Math.floor(100 * elementViewingTimeRegistriesCounter / courseElementsCounter) + '%'
+        // console.log(courseProgress)
+
+        let updateCourseProgress = await Users.updateOne(
+            {
+                code: req.body.studentCode,
+                "groups.groupCode": req.body.groupCode
+            },
+            {
+                $set: { "groups.$.courseProgress": courseProgress }
+            })
+
+        console.log(updateCourseProgress)
+
         context.res = {
             "status": 200,
             "headers": {
