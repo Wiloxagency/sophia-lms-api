36087135a286dc21771458f0f38da5b998d2ac46
Leonardo Barreto <leonardojbarreto@gmail.com>
2023-08-27 20:50:20 -0300

    Cleaning text in srt creation

--- CreateContent/createSrt.ts
+++ CreateContent/createSrt.ts
@@ -10,13 +10,26 @@ const configSrt = {
 }
 
 export async function createSrt(audioUrl: string, textForSubtitles: string, courseCode: string) {
+    console.info(`*${audioUrl}* *${textForSubtitles}* *${courseCode}*`)
+
+    //Cleaning text
+    textForSubtitles = textForSubtitles
+    .replace(/ \./g, '.')
+    .replace(/ \,/g, ',')
+    .replace(/\((\s*)/g, '(')
+    .replace(/(\s*)\)/g, ')')
+    .replace(/\((\s*)/g, '{')
+    .replace(/(\s*)\)/g, '}')
+    .replace(/\((\s*)/g, '[')
+    .replace(/(\s*)\)/g, ']')
+
     try {
         return await axios.post(urlSrt,{audio_url: audioUrl, text_for_subtitles: textForSubtitles}, configSrt).then(async result => { return result.data })
     } catch (error) {
 
-        await saveLog(`Error creating srt for course: ${courseCode}.`, "Error", "createSrt()", "Courses/{courseCode}/CreateContent")
+        await saveLog(`Error creating srt for course: ${courseCode}, error: ` + error.message, "Error", "createSrt()", "x()")
 
-        return [{ data: [] }]
+        return []
     }
 
 }
