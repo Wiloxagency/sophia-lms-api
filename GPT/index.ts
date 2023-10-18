cb2afd3a432c063f5a5ce483e1e8a4ae1000d380
LeoLeto <LeoLeto@proton.me>
2023-10-17 23:23:15 -0300

    Working

--- GPT/index.ts
+++ GPT/index.ts
@@ -11,39 +11,99 @@ const openai = new OpenAIApi(configuration)
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
 
-    try {
-        const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [
-                {
-                    role: "system",
-                    content: 'You are a helpful assistant.'
+    async function chatCompletion() {
+        try {
+            const response = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: [
+                    {
+                        role: "system",
+                        content: 'You are a helpful assistant.'
+                    },
+                    {
+                        role: "user",
+                        content: req.body.prompt
+                    }
+                ]
+            })
+            // console.log(response.data.choices[0].message.content)
+            context.res = {
+                "status": 200,
+                "headers": {
+                    "Content-Type": "application/json"
                 },
-                {
-                    role: "user",
-                    content: req.body.prompt
+                "body": response.data.choices[0].message.content
+            }
+        } catch (error) {
+            await saveLog(`Error creating Chat Completion, error: ${error.message} `, "Error", "AzureFunction()", "GPT")
+            context.res = {
+                "status": 500,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": {
+                    "message": "Error"
                 }
-            ]
-        })
-        // console.log(response.data.choices[0].message.content)
-        context.res = {
-            "status": 200,
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": response.data.choices[0].message.content
-        }
-    } catch (error) {
-        await saveLog(`Error creating Chat Completion, error: ${error.message} `, "Error", "AzureFunction()", "GPT")
-        context.res = {
-            "status": 500,
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": {
-                "message": "Error"
             }
         }
+    }
+
+    async function CreateCourseSummary() {
+        try {
+            const response = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: [
+                    {
+                        role: "system",
+                        content: 'You are a helpful assistant.'
+                    },
+                    {
+                        role: "user",
+                        content: 'You will be given a text delimited by triple quotes.'
+                    },
+                    {
+                        role: "user",
+                        content: 'Text:"""' + req.body.courseTitle + '"""'
+                    },
+                    {
+                        role: "user",
+                        content: 'Create a short description for a course teaching the content of the text. The descrition should be in Spanish.'
+                    },
+                    {
+                        role: "assistant",
+                        content: 'Descripción del curso: '
+                    }
+                ]
+            })
+            // console.log(response.data.choices[0].message.content)
+            context.res = {
+                "status": 200,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": { response: response.data.choices[0].message.content }
+            }
+        } catch (error) {
+            await saveLog(`Error creating course description, error: ${error.message} `, "Error", "AzureFunction()", "GPT")
+            context.res = {
+                "status": 500,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": {
+                    "message": "Error"
+                }
+            }
+        }
+    }
+
+
+
+
+    if (req.query.createCourseSummary) {
+        await CreateCourseSummary()
+    } else {
+        await chatCompletion()
     }
 
 }
