c5a383506a595618a32bf4cda9ddfd70600a1092
LeoLeto <master@wiloxagency.com>
2024-01-23 09:24:55 -0300

    New endpoint base

--- SpeechToTextWebhook/index.ts
+++ SpeechToTextWebhook/index.ts
@@ -2,17 +2,41 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { saveLog } from "../shared/saveLog"
 import { sendScormUnderConstructionEmail } from "../nodemailer/scormDownloadEmail"
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    try {
+const axios = require('axios').default
 
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+
+    try {
         const event = req.headers['x-microsoftspeechservices-event']
         const validationToken = req.query.validationToken
-        
-        sendScormUnderConstructionEmail(
-            "LeoLeto@proton.me",
-            "Leo",
-            JSON.stringify(req.body)
-        )
+
+        if (event == 'Challenge') {
+
+        } else {
+            sendScormUnderConstructionEmail(
+                "LeoLeto@proton.me",
+                "Leo",
+                JSON.stringify(req.body)
+            )
+
+            let config = {
+                method: 'get',
+                maxBodyLength: Infinity,
+                url: req.body.self + '/files',
+                headers: {
+                    'Ocp-Apim-Subscription-Key': '205669de4511412299e6684bb83e5eb1'
+                }
+            }
+
+            axios.request(config)
+                .then((response) => {
+
+                    getTranscriptionResult(response.data.values[0].links.contentUrl)
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        }
 
         context.res = {
             "status": 200,
@@ -22,8 +46,6 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             "body":
                 event == 'Challenge' ? validationToken : event
         }
-
-        return
 
     } catch (error) {
         await saveLog(`Error getting SpeechToTextWebook, error ${error.message} `, "Error", "SpeechToTextWebook()", "SpeechToTextWebook")
@@ -38,6 +60,31 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             }
         }
 
+    }
+
+    async function getTranscriptionResult(transcriptionFileUrl: string) {
+
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: transcriptionFileUrl,
+            headers: {
+                'Ocp-Apim-Subscription-Key': '205669de4511412299e6684bb83e5eb1'
+            }
+        };
+
+        axios.request(config)
+            .then((response) => {
+                // console.log(JSON.stringify(response.data))
+                sendScormUnderConstructionEmail(
+                    "LeoLeto@proton.me",
+                    "Leo",
+                    JSON.stringify(response.data)
+                )
+            })
+            .catch((error) => {
+                console.log(error)
+            })
     }
 }
 
