ac807351a2f3626f742d1b52d980c6213cc988c1
Kelvin <kseidel@edutecno.com>
2024-02-11 12:34:24 -0300

    topicCategorizer fix resp

--- TopicCategorizer/index.ts
+++ TopicCategorizer/index.ts
@@ -2,7 +2,6 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { Configuration, OpenAIApi } from 'openai';
 import { saveLog } from "../shared/saveLog";
 
-// Credenciais do OpenAI
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
@@ -19,26 +18,27 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             messages: [
                 {
                     role: "user",
-                    content: `We have the following topic, '${topic},' based in the context of '${conversationContext}.' Classify it within the most suitable categories considering the following options: 1. Technology 2. Nature 3. Travel 4. Food 5. Health 6. Education 7. People 8. Lifestyle 9. Architecture 10. Animals 11. Art and Culture 12. Sports and Outdoor Activities 13. Fashion 14. Abstract Concepts 15. Backgrounds/Textures. If it cannot be classified within any specific category, use category number 15.\nThe output format must be json with the following structure:\n{'categories': numbers[]}`
+                    content: `We have the following topic, '${topic},' based in the context of '${conversationContext}'. Classify it within the most suitable categories considering the following options: 1. Technology 2. Nature 3. Travel 4. Food 5. Health 6. Education 7. People 8. Lifestyle 9. Architecture 10. Animals 11. Art and Culture 12. Sports and Outdoor Activities 13. Fashion 14. Abstract Concepts 15. Backgrounds/Textures. If it cannot be classified within any specific category, use category number 15.
+                    The output format must be JSON with the following structure:
+                    {'categories': numbers[]}`
                 }
             ]
         });
 
-        // Extrair as categorias da resposta
-        let categories: any = response.data.choices?.[0]?.message?.content;
+        const responseMessage = response.data.choices?.[0]?.message?.content || "";
+        const categoriesRegex = /\d+/g;
+        const extractedCategories = responseMessage.match(categoriesRegex);
+        const categories = extractedCategories?.map(category => parseInt(category, 10)) || [];
 
-        // Retorna a resposta do modelo de linguagem da OpenAI e as categorias
         context.res = {
             status: 200,
             headers: {
                 "Content-Type": "application/json"
             },
-            body: {
-                content: categories
-            }
+            body: { categories: categories }
         };
+
     } catch (error) {
-        // Registra e retorna um erro em caso de falha
         await saveLog(`Error creating Chat Completion, error: ${error.message} `, "Error", "AzureFunction()", "GPT");
         context.res = {
             status: 500,
