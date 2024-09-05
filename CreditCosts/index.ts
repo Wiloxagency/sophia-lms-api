6406557ffbcc78670767c5adf091318ae7f5bc57
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2024-09-05 19:50:15 -0300

    Working update credits

---
+++ CreditCosts/index.ts
@@ -0,0 +1,70 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { createConnection } from "../shared/mongo";
+import { saveLog } from "../shared/saveLog";
+
+const database = createConnection();
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const updateCreditCost = async () => {
+    console.log(req.body);
+
+    try {
+      const db = await database;
+      const CreditCosts = db.collection("creditCosts");
+
+      const updateCreditResponse = await CreditCosts.findOneAndUpdate(
+        { code: req.body.code },
+        { $set: { credits: req.body.credits } }
+      );
+
+      console.log("response: ", updateCreditResponse);
+
+      if (updateCreditResponse) {
+        context.res = {
+          status: 201,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: updateCreditResponse,
+        };
+      } else {
+        context.res = {
+          status: 500,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: {
+            message: "Error updating credit cost",
+          },
+        };
+      }
+    } catch (error) {
+      await saveLog(
+        `Error updating credit cost: ${req.body.code}` + error.message,
+        "Error",
+        "updateCreditCost()",
+        "CreditCost/"
+      );
+      context.res = {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          message: "Error updating credit cost",
+        },
+      };
+    }
+  };
+
+  switch (req.method) {
+    case "PUT":
+      await updateCreditCost();
+      break;
+  }
+};
+
+export default httpTrigger;
