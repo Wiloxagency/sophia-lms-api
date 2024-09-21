750f77839f00762d519563d7f7f0793ca4cf9d72
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2024-09-20 23:39:15 -0300

    Webhook base

--- Stripe/index.ts
+++ Stripe/index.ts
@@ -1,5 +1,8 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import Stripe from "stripe";
+import { createConnection } from "../shared/mongo";
+const database = createConnection();
+
 const STRIPE_SK = process.env.STRIPE_SK;
 const stripe = new Stripe(STRIPE_SK);
 
@@ -8,42 +11,68 @@ const httpTrigger: AzureFunction = async function (
   req: HttpRequest
 ): Promise<void> {
   async function createCheckoutSession() {
-    let customerId = req.body.customerId;
+    try {
+      let stripeCustomerId = req.body.stripeCustomerId;
 
-    if (!customerId) {
-      const customer = await stripe.customers.create({
-        name: req.body.name,
-        email: req.body.email,
+      // IF USER DOESN'T ALREADY HAVE A STRIPE CUSTOMER OBJECT
+      if (!stripeCustomerId) {
+        const customer = await stripe.customers.create({
+          name: req.body.name,
+          email: req.body.email,
+        });
+        stripeCustomerId = customer.id;
+
+        const db = await database;
+        const users = db.collection("user");
+        const user = users.updateOne(
+          { email: req.body.email },
+          { $set: { stripeCustomerId: stripeCustomerId } }
+        );
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        customer: stripeCustomerId,
+        success_url: "https://example.com/success",
+        line_items: [
+          {
+            price: req.body.priceId,
+            quantity: 1,
+          },
+        ],
+        mode: "subscription",
       });
-      customerId = customer.id;
-
-      // UPDATE USER HERE
-    }
-
-    console.log("customer: ", customerId);
-
-    return
-
-    const session = await stripe.checkout.sessions.create({
-      customer: customerId,
-      success_url: "https://example.com/success",
-      line_items: [
-        {
-          // Price for Plan 20.000 Créditos
-          //   price: "price_1PvSk6RsASo6ld22PpIYmBEm",
-          price: req.body.priceId,
-          quantity: 1,
+      context.res = {
+        headers: {
+          "Content-Type": "application/json",
         },
-      ],
-      mode: "subscription",
+        body: { sessionUrl: session.url },
+      };
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function fulfillCheckout() {
+    const sessionId = req.body.sessionId;
+    const stripe = require("stripe")(STRIPE_SK);
+    // TODO: Make this function safe to run multiple times,
+    // even concurrently, with the same session ID
+
+    // TODO: Make sure fulfillment hasn't already been
+    // peformed for this Checkout Session
+
+    // Retrieve the Checkout Session from the API with line_items expanded
+    const checkoutSession = await stripe.checkout.sessions.retrieve(sessionId, {
+      expand: ["line_items"],
     });
-    console.log("session: ");
-    context.res = {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: { sessioUrl: session.url },
-    };
+
+    // Check the Checkout Session's payment_status property
+    // to determine if fulfillment should be peformed
+    if (checkoutSession.payment_status !== "unpaid") {
+      // TODO: Perform fulfillment of the line items
+      // TODO: Record/save fulfillment status for this
+      // Checkout Session
+    }
   }
 
   switch (req.method) {
@@ -51,8 +80,12 @@ const httpTrigger: AzureFunction = async function (
     //   await getSubscriptionPlans();
     //   break;
     case "POST":
-      await createCheckoutSession();
-      break;
+      if (req.query.checkoutSessionCompletedWebhook) {
+        await fulfillCheckout();
+      } else {
+        await createCheckoutSession();
+        break;
+      }
     default:
       break;
   }
