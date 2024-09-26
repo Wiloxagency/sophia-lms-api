3c7934bec41e270bb453bacdbd7ee596eb4c2cd6
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2024-09-25 23:15:30 -0300

    Geolocation debugging 1

--- Geolocation/index.ts
+++ Geolocation/index.ts
@@ -1,7 +1,5 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import axios from "axios";
 import { saveLog } from "../shared/saveLog";
-
 import IPinfoWrapper, { IPinfo, AsnResponse } from "node-ipinfo";
 
 const ipinfoWrapper = new IPinfoWrapper("68191b1738d0d5");
@@ -11,32 +9,21 @@ const httpTrigger: AzureFunction = async function (
   req: HttpRequest
 ): Promise<void> {
   try {
-    console.log(req);
+    const clientIp = req.headers["x-forwarded-for"];
+
+    const ipInfoResponse = ipinfoWrapper
+      .lookupIp(clientIp)
+      .then((response: IPinfo) => {
+        //   console.log(response);
+        return response;
+      });
 
     context.res = {
       headers: {
         "Content-Type": "application/json",
       },
-      body: req,
+      body: { ipInfoResponse },
     };
-    return;
-    const clientIp = req.headers["x-forwarded-for"];
-    context.res = {
-      status: 200,
-      body: { ip: clientIp },
-    };
-    console.log("clientIp: ", clientIp);
-
-    ipinfoWrapper.lookupIp(clientIp).then((response: IPinfo) => {
-      console.log(response);
-    });
-
-    // context.res = {
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: { axiosResponse },
-    // };
   } catch (error) {
     console.error("Error getting geolocation ", error);
     await saveLog(
