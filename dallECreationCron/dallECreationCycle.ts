a119632bacf01069a9a640cba42fcb83b4bcced0
Wiloxagency <master@wiloxagency.com>
2024-09-28 16:10:33 -0300

    Dalle 3 images creation working fine

--- dallECreationCron/dallECreationCycle.ts
+++ dallECreationCron/dallECreationCycle.ts
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import { BlobServiceClient } from "@azure/storage-blob";
 import OpenAI from "openai";
 import sharp = require("sharp");
+import { Db } from "mongodb";
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
@@ -17,23 +18,36 @@ const openAILimit = 190
 // Crear un cliente MongoDB
 const database = createConnection()
 
-const generateImage = async (currentItem, dallePrompt) => {
+const generateImage = async (currentItem, db: Db) => {
 
-    await dallePrompt.updateOne({ _id: currentItem._id }, { $set: { status: "processing" } });
+    await db.collection("dallePrompt").updateOne({ _id: currentItem._id }, { $set: { status: "processing" } });
 
 
     let imageResponse: OpenAI.Images.ImagesResponse
 
+    // Define possible image sizes
+    const imageSizes = [
+        { width: 1024, height: 1024 },
+        { width: 1792, height: 1024 },
+        { width: 1024, height: 1792 }
+    ];
+
+
+
+    // Randomly select an image size
+    const randomNumber = Math.floor(Math.random() * imageSizes.length)
+    const randomSize = imageSizes[randomNumber];
+  
     try {
         imageResponse = await openai.images.generate({
             model: "dall-e-3",
             prompt: currentItem.prompt,
             n: 1,
-            size: "1792x1024",
+            size: randomNumber==0?"1024x1024":randomNumber==1?"1792x1024":"1024x1792",
             response_format: "b64_json",
         });
     } catch (error) {
-        await dallePrompt.updateOne({ _id: currentItem._id }, { $set: { status: "started" } });
+        await db.collection("dallePrompt").updateOne({ _id: currentItem._id }, { $set: { status: "started" } });
         console.log("Dalle error:", error)
         return
     }
@@ -51,17 +65,35 @@ const generateImage = async (currentItem, dallePrompt) => {
         const blobName = uuidv4() + ".jpeg";
         const blockBlobClient = containerClient.getBlockBlobClient(blobName);
         await blockBlobClient.upload(output, output.length);
-        await dallePrompt.updateOne(
+        
+        console.info("saved:", blockBlobClient.url)
+
+        
+
+        let currentParagraphArrayPath = 
+            `sections.${currentItem.sectionIndex}.elements.${currentItem.elementIndex}.elementLesson.paragraphs.${currentItem.slideIndex}.imageData.finalImage`;
+        await  db.collection("course").findOneAndUpdate(
+            { code: currentItem.courseCode },
+            {
+              $set: {
+                [currentParagraphArrayPath]:
+                {
+                    url: blockBlobClient.url,
+                    width: randomSize.width,
+                    height: randomSize.height
+                }
+              },
+            }
+          );
+          await db.collection("dallePrompt").updateOne(
             { _id: currentItem._id }, 
             { $set: { 
                 url: blockBlobClient.url,
                 status: "completed" 
             } });
-        console.info("saved:", blockBlobClient.url)
-        
 
     } catch (error) {
-        await dallePrompt.updateOne({ _id: currentItem._id }, { $set: { status: "started" } });
+        await  db.collection("dallePrompt").updateOne({ _id: currentItem._id }, { $set: { status: "started" } });
         console.log("Blob error:", error)
         return
     }
@@ -84,7 +116,7 @@ export async function aiImageCreation() {
         return
 
     imagesInList.forEach(element => {
-        generateImage(element, dallePrompt)
+        generateImage(element, db)
     })
 
 
